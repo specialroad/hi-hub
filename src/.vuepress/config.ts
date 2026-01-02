2bc1dffb515a14e51a1050458597409c7741e758
specialroad <3162433464@qq.com>
2026-01-02 15:17:38 +0800

    修改博客名称

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/hi-hub/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "VitaLab",
+  description: "Simple Code, Deep Life.",
 
   theme,
 
